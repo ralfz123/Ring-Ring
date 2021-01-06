c862ecbc783b88f723345aa088bfa3076d8b760b
ralfz123 <ralf_zonneveld@hotmail.com>
2021-01-06 13:34:31 +0100

    Working barchart and grid-auto-rows added

--- script/barchart.js
+++ script/barchart.js
@@ -1,39 +1,74 @@
 var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
+var myBarChart = new Chart(ctx, {
 	type: 'bar',
 	data: {
-		labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+		labels: ['0-18', '18-30', '30-65', '65+'],
 		datasets: [
 			{
-				label: '# of Votes',
-				data: [12, 19, 3, 5, 2, 3],
-				backgroundColor: [
-					'rgba(255, 99, 132, 0.2)',
-					'rgba(54, 162, 235, 0.2)',
-					'rgba(255, 206, 86, 0.2)',
-					'rgba(75, 192, 192, 0.2)',
-					'rgba(153, 102, 255, 0.2)',
-					'rgba(255, 159, 64, 0.2)',
-				],
-				borderColor: [
-					'rgba(255, 99, 132, 1)',
-					'rgba(54, 162, 235, 1)',
-					'rgba(255, 206, 86, 1)',
-					'rgba(75, 192, 192, 1)',
-					'rgba(153, 102, 255, 1)',
-					'rgba(255, 159, 64, 1)',
-				],
+				label: 'Man',
+				backgroundColor: '#5CA9BD',
+				data: [3, 7, 4, 6],
+			},
+			{
+				label: 'Vrouw',
+				backgroundColor: '#576399',
+				data: [4, 3, 5, 8],
 				borderWidth: 1,
 			},
 		],
 	},
 	options: {
+		plugins: {
+			datalabels: {
+                anchor: 'center',
+                color: 'white'
+			},
+		},
+
+		// Datavalues on top of bar - https://stackoverflow.com/a/42558204
+		// animation: {
+		// 	duration: 1,
+		// 	onComplete: function () {
+		// 		var chartInstance = this.chart,
+		// 			ctx = chartInstance.ctx;
+
+		// 		ctx.font = Chart.helpers.fontString(
+		// 			Chart.defaults.global.defaultFontSize,
+		// 			Chart.defaults.global.defaultFontStyle,
+		// 			Chart.defaults.global.defaultFontFamily
+		// 		);
+		// 		ctx.textAlign = 'center';
+		// 		ctx.textBaseline = 'bottom';
+
+		// 		this.data.datasets.forEach(function (dataset, i) {
+		// 			var meta = chartInstance.controller.getDatasetMeta(i);
+		// 			meta.data.forEach(function (bar, index) {
+		// 				var data = dataset.data[index];
+		// 				ctx.fillText(data, bar._model.x, bar._model.y - 5);
+		// 			});
+		// 		});
+		// 	},
+		// },
+		barValueSpacing: 20,
 		scales: {
 			yAxes: [
 				{
+					scaleLabel: {
+						display: true,
+						labelString: 'Ongelukken',
+					},
 					ticks: {
 						beginAtZero: true,
 					},
+				},
+			],
+			xAxes: [
+				{
+                    scaleLabel: {
+						display: true,
+						labelString: 'Leeftijd',
+					},
+                    barPercentage: 1,
 				},
 			],
 		},
