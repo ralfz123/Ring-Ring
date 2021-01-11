eab5e36e0e0d9f5603bf87f9b4a1b657803236ec
ralfz123 <ralf_zonneveld@hotmail.com>
2021-01-11 15:15:48 +0100

    cycle routes are plotted on map

--- src/script/visuals/map.js
+++ src/script/visuals/map.js
@@ -1,10 +1,31 @@
-// ***
 mapboxgl.accessToken =
 	'pk.eyJ1IjoicmFsZjg4IiwiYSI6ImNrampzODg5MDFteTYycW83cWVoenZtaTgifQ.kLGZ-0NFqYzEtDLLEY5NbQ';
-var map = new mapboxgl.Map({
+const map = new mapboxgl.Map({
 	container: 'map',
-    // style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-    style: 'mapbox://styles/ralf88/ckjjswhgo0xlt19nxxqoky3dy',
-	center: [4.9, 52.38], // starting position [lng, lat]
-    zoom: 11.4, // starting zoom
+	// style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+	style: 'mapbox://styles/ralf88/ckjjswhgo0xlt19nxxqoky3dy',
+	center: [4.9, 52.38], 
+	zoom: 11.4, 
+});
+
+// https://cors-anywhere.herokuapp.com/http://ringring.jorrr.nl/geojson-data-ringring.json
+
+map.on('load', function () {
+	map.addSource('routes', {
+		type: 'geojson',
+		data: 'https://cors-anywhere.herokuapp.com/http://ringring.jorrr.nl/geojson-data-ringring.json'
+	});
+	map.addLayer({
+		id: 'route',
+        type: 'line',
+        source: 'routes',
+		layout: {
+			'line-join': 'round',
+			'line-cap': 'round',
+		},
+		paint: {
+			'line-color': '#928',
+			'line-width': 1,
+		},
+	});
 });
