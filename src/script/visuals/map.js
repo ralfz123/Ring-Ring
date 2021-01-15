96efa7a5a03179c3bb493c683c2a7cfa1eac7779
ralfz123 <ralf_zonneveld@hotmail.com>
2021-01-15 15:01:41 +0100

    Tooltip is dynamic

--- src/script/visuals/map.js
+++ src/script/visuals/map.js
@@ -10,7 +10,7 @@ const map = new mapboxgl.Map({
 	// style: 'mapbox://styles/ralf88/ckjjswhgo0xlt19nxxqoky3dy',
 	style: 'mapbox://styles/ralf88/ckjtxwzmu0si719mwyiayscvr',
 	center: [4.9, 52.38],
-	zoom: 11.4,
+	zoom: 10,
 });
 
 // -----------------------------------------------------------------------------------------------------------------------------
@@ -51,7 +51,7 @@ map.on('load', function () {
 	});
 
 	// fetch external API and then put it in the data var below, then it can be plotted as expected
-	map.addSource('accidents', {
+	map.addSource('outcome', {
 		type: 'geojson',
 		data:
 			'https://gist.githubusercontent.com/ralfz123/506502be46c91f5b5951e85f1b7c665b/raw/d64f127cd93cac901739745cfdb7807b776b6506/accidents_data.geojson',
@@ -72,13 +72,14 @@ map.on('load', function () {
 	});
 
 	map.addLayer({
-		id: 'accidents',
+		id: 'accidents-viz',
 		type: 'circle',
-		source: 'accidents',
+		source: 'outcome',
 		paint: {
 			// 'circle-stroke-color': 'blue',
-			// 'circle-stroke-width': 3,
-			'circle-color': '#ce3636',
+			// 'circle-stroke-width': 1,
+			'circle-color': 'white',
+			// Zoom in at scrolling
 			// 'circle-radius': {
 			// 	'base': 1.75,
 			// 	'stops': [
@@ -86,53 +87,98 @@ map.on('load', function () {
 			// 	[22, 180]
 			// 	]
 			// 	},
+
+			'circle-color': [
+				'match',
+				['get', 'outcome'],
+				['Dodelijk', 'dodelijk'],
+				'#ce3636',
+				['Gewond', 'gewond'],
+				'#e35e0d',
+
+				/* fallback */ '#ccc',
+			],
 		},
+	});
+
+	var quakeID = null;
+
+
+
+
+
+	map.on('mousemove', 'accidents-viz', (e) => {
+		map.getCanvas().style.cursor = 'pointer';
+		// Set variables equal to the current feature's magnitude, location, and time
+		var accidentOutcome = e.features[0].properties.outcome;
+		var accidentStreet = e.features[0].properties.street;
+		var accidentAge = e.features[0].properties.age;
+		// var accidentImage = e.features[0].properties.image_article_site;
+		// var accidentUrl = e.features[0].properties.url_site;
+
+		// Check whether features exist
+		if (e.features.length > 0) {
+
+			let outcome = document.getElementById('tooltip-data-accident');
+			let streetname = document.getElementById('tooltip-data-streetname');
+			let age = document.getElementById('tooltip-data-age');
+
+
+			// Display the magnitude, location, and time in the sidebar
+			outcome.textContent = accidentOutcome;
+			streetname.textContent = accidentStreet;
+			age.textContent = accidentAge;
+			// magDisplay.textContent = accidentImage;
+			// magDisplay.textContent = accidentUrl;
+
+
+						
+
+			// If quakeID for the hovered feature is not null,
+			// use removeFeatureState to reset to the default behavior
+			if (quakeID) {
+				map.removeFeatureState({
+					source: 'earthquakes',
+					id: quakeID,
+				});
+			}
+
+			quakeID = e.features[0].id;
+
+			// When the mouse moves over the earthquakes-viz layer, update the
+			// feature state for the feature under the mouse
+			map.setFeatureState(
+				{
+					source: 'earthquakes',
+					id: quakeID,
+				},
+				{
+					hover: true,
+				}
+			);
+		}
 	});
 });
 
+// ------------------ SCRIPTING / FUNCTIONS ------------------------------------------------------------
 
+// scripting function
+// HTML elements that will be dynamic
+// let outcome = document.getElementById('tooltip-data-accident');
+// let streetname = document.getElementById('tooltip-data-streetname');
+// let age = document.getElementById('tooltip-data-age');
+// let rainfall = document.getElementById('tooltip-data-rainfall');
+// let wind = document.getElementById('tooltip-data-wind');
+
+// Function that replace the values of the HTML elements with dynamic data
+// streetname.innerHTML = dataStreetname;
+// age.innerHTML = dataAge;
+// rainfall.innerHTML = dataRainfall;
+// wind.innerHTML = dataWind;
+
+// ------------------------------------------------------------------------------
 
 // data dynamic 🚨 - https://docs.mapbox.com/help/tutorials/create-interactive-hover-effects-with-mapbox-gl-js/#define-the-hover-attribute
-
-// var quakeID = null;
-
-// map.on('mousemove', 'earthquakes-viz', (e) => {
-
-//   map.getCanvas().style.cursor = 'pointer';
-//   // Set variables equal to the current feature's magnitude, location, and time
-//   var quakeMagnitude = e.features[0].properties.mag;
-//   var quakeLocation = e.features[0].properties.place;
-//   var quakeDate = new Date(e.features[0].properties.time);
-
-//   // Check whether features exist
-//   if (e.features.length > 0) {
-//     // Display the magnitude, location, and time in the sidebar
-//     magDisplay.textContent = quakeMagnitude;
-//     locDisplay.textContent = quakeLocation;
-//     dateDisplay.textContent = quakeDate;
-
-//     // If quakeID for the hovered feature is not null,
-//     // use removeFeatureState to reset to the default behavior
-//     if (quakeID) {
-//       map.removeFeatureState({
-//         source: "earthquakes",
-//         id: quakeID
-//       });
-//     }
-
-//     quakeID = e.features[0].id;
-
-//     // When the mouse moves over the earthquakes-viz layer, update the
-//     // feature state for the feature under the mouse
-//     map.setFeatureState({
-//       source: 'earthquakes',
-//       id: quakeID,
-//     }, {
-//       hover: true
-//     });
-
-//   }
-// });
 
 // -----------------------------------------------------------------------------------------------------------------------------
 
@@ -182,26 +228,3 @@ const endpointTwo =
 // 	console.log('cycleAccidentsData', cycleAccidentsData)
 
 // for loop
-
-// ------------------ SCRIPTING / FUNCTIONS ------------------------------------------------------------
-
-// scripting function
-// HTML elements that will be dynamic
-let streetname = document.getElementById('tooltip-data-streetname');
-let age = document.getElementById('tooltip-data-age');
-let rainfall = document.getElementById('tooltip-data-rainfall');
-let wind = document.getElementById('tooltip-data-wind');
-
-// Example data
-let dataStreetname = 'Karolano';
-let dataAge = 30;
-let dataRainfall = 013;
-let dataWind = 10000;
-
-// Function that replace the values of the HTML elements with dynamic data
-streetname.innerHTML = dataStreetname;
-age.innerHTML = dataAge;
-rainfall.innerHTML = dataRainfall;
-wind.innerHTML = dataWind;
-
-// ------------------------------------------------------------------------------
