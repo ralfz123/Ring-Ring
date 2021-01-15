14f4b5b3fa9c45ba36aa50ad193c32a01f7d80cc
ralfz123 <ralf_zonneveld@hotmail.com>
2021-01-15 22:59:36 +0100

    Code added for toggling between datasets

--- src/script/visuals/map.js
+++ src/script/visuals/map.js
@@ -29,6 +29,7 @@ map.on('load', function () {
 		type: 'line',
 		source: 'routes',
 		layout: {
+			'visibility': 'visible',
 			'line-join': 'round',
 			'line-cap': 'round',
 		},
@@ -163,4 +164,44 @@ map.on('load', function () {
 	map.on('mouseleave', 'accidents-viz', function () {
 		map.getCanvas().style.cursor = 'grab';
 	});
+
+	// Hide and show data layer
+	// https://docs.mapbox.com/mapbox-gl-js/example/toggle-layers/
+	// map.on('click', 'routes', (e) => {
+	// 	document.getElementById('filter-pattern-dataset-toggle-button');
+	// 	map.getCanvas().style.display = 'none';
+	// });
+
+	// enumerate ids of the layers
+	var toggleableLayerIds = ['route'];
+
+	// set up the corresponding toggle button for each layer
+	for (var i = 0; i < toggleableLayerIds.length; i++) {
+		var id = toggleableLayerIds[i];
+
+		var link = document.createElement('a');
+		link.href = '#';
+		link.className = 'active';
+		link.textContent = id;
+
+		link.onclick = function (e) {
+			var clickedLayer = this.textContent;
+			e.preventDefault();
+			e.stopPropagation();
+
+			var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+
+			// toggle layer visibility by changing the layout object's visibility property
+			if (visibility === 'visible') {
+				map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+				this.className = '';
+			} else {
+				this.className = 'active';
+				map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+			}
+		};
+
+		var layers = document.getElementById('menu');
+		layers.appendChild(link);
+	}
 });
